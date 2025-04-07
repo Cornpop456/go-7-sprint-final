972ac823243b34cae5b2c108d56d004276d721c6
Victor Ginzburg <v-viktor456@mail.ru>
2025-04-07 11:56:40 +0300

    first iteration

--- main_test.go
+++ main_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCafeWhenOk(t *testing.T) {
@@ -24,7 +25,7 @@ func TestCafeWhenOk(t *testing.T) {
 
 		handler.ServeHTTP(response, req)
 
-		assert.Equal(t, http.StatusOK, response.Code)
+		require.Equal(t, http.StatusOK, response.Code)
 		// пока сравнивать не будем, а просто выведем ответы
 		// удалите потом этот вывод
 		fmt.Println(response.Body.String())
@@ -49,7 +50,75 @@ func TestCafeNotOk(t *testing.T) {
 
 		handler.ServeHTTP(response, req)
 
-		assert.Equal(t, v.statusCodeWant, response.Code)
+		require.Equal(t, v.statusCodeWant, response.Code)
 		assert.Equal(t, v.bodyWant, strings.TrimSpace(response.Body.String()))
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	city := "moscow"
+	wantedStatus := http.StatusOK
+
+	requests := []struct {
+		count int
+		want  int
+	}{
+		{count: 0, want: 0},
+		{count: 1, want: 1},
+		{count: 2, want: 2},
+		{count: 100, want: min(len(cafeList[city]), 100)},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", fmt.Sprintf("/cafe?city=%s&count=%d", city, v.count), nil)
+
+		handler.ServeHTTP(response, req)
+
+		cities := strings.Split(strings.TrimSpace(response.Body.String()), ",")
+
+		if cities[0] == "" {
+			cities = cities[1:]
+		}
+
+		require.Equal(t, wantedStatus, response.Code)
+		assert.Equal(t, v.want, len(cities))
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	city := "moscow"
+	wantedStatus := http.StatusOK
+
+	requests := []struct {
+		search    string
+		wantCount int
+	}{
+		{search: "фасоль", wantCount: 0},
+		{search: "кофе", wantCount: 2},
+		{search: "вилка", wantCount: 1},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", fmt.Sprintf("/cafe?city=%s&search=%s", city, v.search), nil)
+
+		handler.ServeHTTP(response, req)
+
+		cities := strings.Split(strings.TrimSpace(response.Body.String()), ",")
+
+		if cities[0] == "" {
+			cities = cities[1:]
+		}
+
+		require.Equal(t, wantedStatus, response.Code)
+
+		for _, city := range cities {
+			assert.True(t, strings.Contains(strings.ToLower(city), strings.ToLower(v.search)))
+		}
+
+		assert.Equal(t, v.wantCount, len(cities))
 	}
 }
